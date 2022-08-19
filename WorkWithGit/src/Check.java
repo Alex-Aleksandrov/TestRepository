446cb35f6c89765cfd85c0eaa6d729efbb4b65d3
Alex Alexandrov <alex2020alexandrov@mail.ru>
2022-08-19 17:33:05 +0300

    Hello

--- WorkWithGit/src/Check.java
+++ WorkWithGit/src/Check.java
@@ -1,6 +1,6 @@
 public class Check {
         public static Integer test(Integer sum) {
-            count = sum + 3;    
+            Integer count = sum + 3;
             return count;
         }
 }
